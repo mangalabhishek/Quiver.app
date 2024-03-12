68a98a4669534b90dbe0c343ea68021381ea3cc9
Abhishek Mangal <201b008@juetguna.in>
2024-03-12 12:44:06 +0530

    implement all the features

--- components/forms/PostThread.tsx
+++ components/forms/PostThread.tsx
@@ -7,7 +7,7 @@ import { Textarea } from "@/components/ui/textarea"
 import {zodResolver} from '@hookform/resolvers/zod';
 import * as z from "zod"
 import { usePathname, useRouter } from 'next/navigation';
-
+import { useOrganization } from '@clerk/nextjs'; 
 // import { updateUser } from '@/lib/actions/user.actions';
 import { ThreadValidation } from '@/lib/validations/thread';
 import { createThread } from '@/lib/actions/thread.actions';
@@ -29,6 +29,7 @@ function PostThread({ userId } : { userId : string }){
     
     const router = useRouter();
     const pathname = usePathname();
+    const {organization} = useOrganization();
     
     const form =useForm({
         resolver: zodResolver(ThreadValidation),
@@ -42,9 +43,9 @@ function PostThread({ userId } : { userId : string }){
         await createThread({
             text: values.thread,
             author:userId,
-            communityId: null,
+            communityId: organization ? organization.id : null,
             path:pathname
-        });
+        });    
 
         router.push("/")
     }
